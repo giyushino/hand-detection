e620da03cf2a3d98dad23780f484b52ea70f6c8b
giyushino <allanzhang440@gmail.com>
2024-09-21 23:44:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from ultralytics import YOLO
 import torch
 
 def main():
-    model = YOLO(r"C:\Users\allan\PycharmProjects\yolov8\runs\detect\train8\weights\best.pt")
-
+    model = YOLO(r"path to best.pt")
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     results = model.train(
-        data=r"C:\Users\allan\PycharmProjects\yolov8\config.yaml",
+        data=r"path to config.yaml",
         epochs=300,
         patience=100,
-        project=r"C:\Users\allan\PycharmProjects\yolov8\runs\detect",
+        project=r"path to \yolov8\runs\detect",
         device=device,
         cache=False  # Force cache regeneration
     )
